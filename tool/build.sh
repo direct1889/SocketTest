21806503c0fe4fa02888446492ac7b9397daed67
direct1889 <s969.6261asthastnkzgfk@gmail.com>
2021-04-24 21:19:05 +0900

    feat: 定数化ノススメ

--- tool/build.sh
+++ tool/build.sh
@@ -4,28 +4,33 @@ cd `dirname $0`
 
 echo "build start"
 
+SRC_DIR="../src/"
+EXE_DIR="../exe/"
+CPP_VERSION=17
+CPP_COMMONS=""
+
 NAME="LegacyServer"
 echo "build ${NAME}..."
-clang++ -std=c++17 "../src/${NAME}.cpp" -o "../exe/${NAME}Cpp"
+clang++ "-std=c++${CPP_VERSION}" "${SRC_DIR}${NAME}.cpp" -o "${EXE_DIR}${NAME}Cpp"
 
 NAME="LegacyClient"
 echo "build ${NAME}..."
-clang++ -std=c++17 "../src/${NAME}.cpp" -o "../exe/${NAME}Cpp"
+clang++ "-std=c++${CPP_VERSION}" "${SRC_DIR}${NAME}.cpp" -o "${EXE_DIR}${NAME}Cpp"
 
 NAME="SimpleServer"
 echo "build ${NAME}..."
-clang++ -std=c++17 "../src/${NAME}.cpp" -o "../exe/${NAME}Cpp"
+clang++ "-std=c++${CPP_VERSION}" "${SRC_DIR}${NAME}.cpp" -o "${EXE_DIR}${NAME}Cpp"
 
 NAME="SimpleClient"
 echo "build ${NAME}..."
-clang++ -std=c++17 "../src/${NAME}.cpp" -o "../exe/${NAME}Cpp"
+clang++ "-std=c++${CPP_VERSION}" "${SRC_DIR}${NAME}.cpp" -o "${EXE_DIR}${NAME}Cpp"
 
 NAME="Server"
 echo "build ${NAME}..."
-mcs "../src/${NAME}.cs" "-out:../exe/${NAME}Cs.exe"
+mcs "${SRC_DIR}${NAME}.cs" "-out:${EXE_DIR}${NAME}Cs.exe"
 
 NAME="Client"
 echo "build ${NAME}..."
-mcs "../src/${NAME}.cs" "-out:../exe/${NAME}Cs.exe"
+mcs "${SRC_DIR}${NAME}.cs" "-out:${EXE_DIR}${NAME}Cs.exe"
 
 echo "build finished"
